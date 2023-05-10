3b91c8e824bd316a189a480b0904e1af064fd8f6
miguel <miguelconde121004@gmail.com>
2023-05-10 16:15:04 -0300

    deletei a velha pasta venv e adicionei outra por não estar executando o comando flask run

--- meuprojeto/venv/Scripts/activate.bat
+++ meuprojeto/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Fatec\Desktop\Nova pasta\meuprojeto\venv
+set VIRTUAL_ENV=C:\Users\migue\OneDrive\Área de Trabalho\Nova pasta (3)\meuprojeto\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
